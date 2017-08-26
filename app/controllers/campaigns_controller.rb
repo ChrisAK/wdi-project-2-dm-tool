c5a4f2d11765de220b0607d4cd2ae568ef0b46f5
ChrisAK <ckennelly8910@gmail.com>
2017-08-26 13:40:40 -0400

    Test user.Campaign.all in campaign controller, line 6

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,7 +3,7 @@ class CampaignsController < ProtectedController
 
   # GET /campaigns
   def index
-    @campaigns = Campaign.all
+    @campaigns = @user.Campaign.all
 
     render json: @campaigns
   end
